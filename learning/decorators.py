d2becca6ecefaa19d10e9b1daa5db0b21efbb408
Michael Babin <iamsuperman@ukr.net>
2019-02-05 10:11:37 +0200

    added input data transformation file

--- learning/decorators.py
+++ learning/decorators.py
@@ -8,7 +8,7 @@ def timelog(message):
             start = time.perf_counter()
             print(f"{message} started...")
             result = func(*args, **kwargs)
-            print(f"{message} finished in {time.perf_counter() - start} seconds")
+            print(f"{message} finished in {(time.perf_counter() - start):.3f} seconds")
             return result
         return wrapped
     return decorator
